07b89a0a637966f353a82bd420a6ffb412ec1661
Richard Mbingi <richard.mbingi@strathmore.edu>
2019-12-10 18:51:19 +0300

    User uthentication service

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,51 @@
+  
 import 'package:flutter/material.dart';
-import 'package:food_plus/screens/screens.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:food_plus/services/services.dart';
+import 'services/services.dart';
+import 'screens/screens.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login Screen',
-      theme: ThemeData(primarySwatch: Colors.teal),
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return MultiProvider(
+      providers: [
+        StreamProvider<FirebaseUser>.value(value: AuthService().user)
+      ],
+      child: MaterialApp(
+        //? Firebase Analytics
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
+        ],
+
+        //? Named Routes
+        routes: {
+          '/': (context) => LoginScreen(),
+          '/home': (context) => HomeScreen(),
+        },
+
+        //? Theme
+        theme: ThemeData(
+          fontFamily: 'Nunito',
+          bottomAppBarTheme: BottomAppBarTheme(
+            color: Colors.black87,
+          ),
+          brightness: Brightness.dark,
+          textTheme: TextTheme(
+            body1: TextStyle(fontSize: 18),
+            body2: TextStyle(fontSize: 16),
+            button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
+            headline: TextStyle(fontWeight: FontWeight.bold),
+            subhead: TextStyle(color: Colors.grey),
+          ),
+          buttonTheme: ButtonThemeData(),
+        ),
+      ),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key key}) : super(key: key);
 
-class HomePage extends StatefulWidget {
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       appBar: AppBar(
-         title: Text("Login Screen"),
-        ),
-       body: Container(),
+    return Container(
+     
     );
   }
 }
